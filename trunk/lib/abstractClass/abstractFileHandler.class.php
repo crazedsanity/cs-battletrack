15f4f5db7406b65a76149a3a32ff165fa7452f05
crazedsanity <crazedsanity@4b77e13d-ec49-0410-873c-e52f5c533875>
2008-04-03 01:20:33 +0000

    ability to delete files (destroy_file()), updated headers & logic.

--- trunk/lib/abstractClass/abstractFileHandler.class.php
+++ trunk/lib/abstractClass/abstractFileHandler.class.php
@@ -23,15 +23,14 @@ abstract class abstractFileHandler {
 	/**
 	 * Write data into a file (or create an empty file, as in a lockfile).
 	 * 
-	 * @param $filename	(str) filename to create (can have existing dir + file, 
-	 * 						such as "existingDir/filename.txt").
-	 * @param $data		(str,optional) data to write into filename.
+	 * @param $filename		(str) filename to create (can have existing dir + file, 
+	 * 							such as "existingDir/filename.txt").
+	 * @param $data			(str,optional) data to write into filename.
 	 * 
-	 * @return NULL		FAIL: no data written
-	 * @return (int)	PASS: indicates number of bytes written (0 is okay if $data is null).
+	 * @return (int)		PASS: indicates number of bytes written (0 is okay if $data is null).
+	 * @return (exception)	FAIL: exception indicates the problem.
 	 */
 	protected function create_file($filename, $data=NULL) {
-		$retval = NULL;
 		if($this->fs->create_file($filename, TRUE)) {
 			$retval = 0;
 			if(!is_null($data) && strlen($data)) {
@@ -41,6 +40,7 @@ abstract class abstractFileHandler {
 		else {
 			throw new exception(__METHOD__ .": failed to create (". $filename .")");
 		}
+		$this->fs->closeFile();
 		return($retval);
 	}//end create_file()
 	//=========================================================================
@@ -53,11 +53,37 @@ abstract class abstractFileHandler {
 	 * 
 	 * @param $filename		(str) filename to read.
 	 * 
-	 * @return (str)		Contains all data from within the given filename
+	 * @return (str)		PASS: Contains all data from within the given filename
+	 * @return (exception)	FAIL: cs_fileSystemClass ran into a problem.
 	 */
 	protected function read_file($filename) {
-		return($this->fs->read($filename));
+		$retval = $this->fs->read($filename);
+		$this->fs->closeFile();
+		return($retval);
 	}//end read_file()
+	//=========================================================================
+	
+	
+	
+	//=========================================================================
+	/**
+	 * Delete a file.
+	 * 
+	 * @param $filename		(str) filename to delete.
+	 * 
+	 * @return (NULL)		PASS: no news is good news.
+	 * @return (exception)	FAIL: exception indicates error.
+	 */
+	protected function destroy_file($filename) {
+		if($this->fs->rm($filename)) {
+			$retval = TRUE;
+		}
+		else {
+			throw new exception(__METHOD__ .": failed to destroy (". $filename .")");
+		}
+		
+		return($retval);
+	}//end destroy_file()
 	//=========================================================================
 }
 ?>
