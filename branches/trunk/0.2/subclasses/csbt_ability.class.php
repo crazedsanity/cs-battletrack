b9ba675b8915e54cbfe372d6fdbdb9301e56ba36
crazedsanity <crazedsanity@4b77e13d-ec49-0410-873c-e52f5c533875>
2010-07-15 20:21:14 +0000

    For newer (very dramatic) changes, where all data is stored in more specialized tables.

---
+++ branches/trunk/0.2/subclasses/csbt_ability.class.php
@@ -0,0 +1,115 @@
+<?php 
+
+/*
+ *  SVN INFORMATION::::
+ * --------------------------
+ * $HeadURL$
+ * $Id$
+ * $LastChangedDate$
+ * $LastChangedRevision$
+ * $LastChangedBy$
+ */
+
+class csbt_ability extends cs_singleTableHandlerAbstract	 {
+	
+	protected $characterId;
+	protected $fields;
+	
+	/** Did you notice "{tableName}_{pkeyField}_seq"? PostgreSQL makes that simple, others don't.*/
+	const tableName = 'csbt_ability_table';
+	const tableSeq  = 'csbt_ability_table_ability_id_seq';
+	const pkeyField = 'ability_id';
+	
+	protected $dataCache=array();
+	
+	//-------------------------------------------------------------------------
+	/**
+	 */
+	public function __construct(cs_phpDB $dbObj) {
+		$this->fields = array(
+			'ability_id'		=> 'int',
+			'ability_name'		=> 'sql'
+		);
+		
+		parent::__construct($dbObj, self::tableName, self::tableSeq, self::pkeyField, $this->fields);
+		$this->get_ability_list();
+	}//end __construct()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function get_ability_list() {
+		try {
+			$data = $this->get_records();
+			
+			foreach($data as $id=>$info) {
+				$this->dataCache['byId'][$id] = $info['ability_name'];
+				$this->dataCache['byName'][$info['ability_name']] = $id;
+			}
+			
+			if(count($this->dataCache['byId']) !== count($this->dataCache['byName'])) {
+				$this->gfObj->debug_print($data,1);
+				$this->gfObj->debug_print($this->dataCache,1);
+				throw new exception(__METHOD__ .":: FATAL ERROR: couldn't get lists to line-up, so the kittens ate their mittens... ");
+			}
+		}
+		catch(Exception $e) {
+			throw new exception(__METHOD__ .":: FATAL ERROR::: ". $e->getMessage());
+		}
+		
+		return($this->dataCache);
+	}//end get_ability_list()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function get_ability_name($id) {
+		if(isset($this->dataCache['byId'][$id])) {
+			$retval = $this->dataCache['byId'][$id];
+		}
+		else {
+			throw new exception(__METHOD__ .":: invalid id (". $id .")");
+		}
+		
+		return($retval);
+	}//end get_ability_name()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function get_ability_id($name) {
+		if(isset($this->dataCache['byName'][$name])) {
+			$retval = $this->dataCache['byName'][$name];
+		}
+		else {
+			$this->gfObj->debug_print($this->dataCache,1);
+			throw new exception(__METHOD__ .":: invalid name (". $name .")");
+		}
+		
+		return($retval);
+	}//end get_ability_id()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function get_ability_modifier($score) {
+		//TODO: handle temporary modifiers ($isTemp)
+		if(is_numeric($score) && $score > 0) {
+			$modifier = floor(($score -10)/2);
+		}
+		elseif(is_null($score)) {
+			$modifier = null;
+		}
+		else {
+			throw new exception(__METHOD__ .":: invalid score (". $score .")");
+		}
+		return($modifier);
+	}//get_ability_modifier()
+	//-------------------------------------------------------------------------
+}
+
+?>
