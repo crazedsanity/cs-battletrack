89f51f077a1db8717716a5619728cbe0e2ed2802
crazedsanity <crazedsanity@4b77e13d-ec49-0410-873c-e52f5c533875>
2010-10-11 22:46:04 +0000

    More changes for handling dirty/processing/updated inputs & parsing.
    NOTE::: many changes are intermediary and have plenty of commented-out code 
    or other debugging stuff.
    
    /sample_scripts/css/dnd.css:
    	* added row coloring styles for weapons & armor
    /sample_scripts/js/dnd.js:
    	* Changes to "dirty inputs":
    		-- a field (input/textarea/select) that has been changed will show as 
    		light blue (easier to see text while typing)
    		-- a field being updated is red & marked as read-only.
    		-- a field that was successfully updated shows as yellow (as before)
    	* For lists, such as gear, special abilities, and skills, there's some 
    	in-progress stuff for re-populating it; the AJAX code still needs to be 
    	updated so it sends back HTML to replace the old stuff with.
    	* cope with XML tags that are empty: the value should be changed to a 
    	zero-length string.
    	* use a couple of nifty calls in document.ready to determine if a value 
    	has changed or not (though code has to be added so the "last_value" part 
    	gets updated once the change is submitted).
    /sample_scripts/templates/character/sheet.content.tmpl:
    	* changed things from just readonly to disabled (easier to determine 
    	what is able to be updated & what isn't).
    	* fixed ID for strength modifier.
    	* added some classes for easier CSS+JS styling/updating.
    	* changes to vars so the select ability drop-down parses properly.

--- trunk/0.5/sample_scripts/js/dnd.js
+++ trunk/0.5/sample_scripts/js/dnd.js
@@ -1,27 +1,80 @@
 
+var originalValue = [];
 
-var originalValue = null;
 function markDirtyInput(object) {
-	/*
-	if($(object).hasClass('redFade')) {
-		$(object).removeClass('redFade');
-	}
-	*/
-	$(object).addClass('redBg');
+	clearUpdatedInput(object);
+	$(object).addClass('dirtyInput');
 }
 function clearDirtyInput(object) {
-	if($(object).hasClass('redBg')) {
-		$(object).removeClass('redBg');
+	if($(object).hasClass('dirtyInput')) {
+		$(object).removeClass('dirtyInput');
 	}
-	//$(object).addClass('redFade');
-	
+	if($(object).attr("readonly") == true) {
+		$(object).attr("readonly",false);
+	}
+	clearUpdatedInput(object);
+	clearProcessingInput(object);
 }
 function isDirtyInput(object) {
 	var retval = false;
-	if($(object).hasClass('redBg')) {
+	if($(object).hasClass('dirtyInput')) {
 		retval = true;
 	}
 	return(retval);
+}
+function markUpdatedInput(object, newVal, forceChange) {
+	if(forceChange == undefined || forceChange == null) {
+		forceChange=false;
+	}
+	clearDirtyInput(object);
+	if($(object).val() != newVal || forceChange) {
+		$(object).val(newVal);
+		$(object).addClass("updatedInput");
+	}
+	if($(object).attr("id") == 'main__character_name') {
+		//
+		var bits = document.title.split(" - ");
+		document.title = $("#main__character_name").val() + " - " + bits[1];
+	}
+}
+function clearUpdatedInput(object) {
+	if($(object).hasClass('updatedInput')) {
+		$(object).removeClass('updatedInput');
+	}
+}
+function isUpdatedInput(object) {
+	var retval = false;
+	if($(object).hasClass('updatedInput')) {
+		retval = true;
+	}
+	return(retval);
+}
+
+function markProcessingInput(object) {
+	if($(object).attr('id').match(/__new__/)) {
+		//special handling for "NEW" records...
+		//If the ID is "gear__new__stuff", this will remove the "stuff" table & replace it with a loading image.
+		var bits = $(object).attr('id').split('__');
+		$("#"+ bits[2]).slideUp();
+		$("#"+ bits[2] +" input").attr("disabled", "disabled");
+		$("#"+ bits[2]).html('<img src="/images/ajax-loader.gif">Loading new '+ bits[2]);
+		$("#"+ bits[2]).slideDown();
+	}
+	else {
+		$(object).addClass("processingInput");
+	}
+}
+function isProcessingInput(object) {
+	var retval = false;
+	if($(object).hasClass("processingInput")) {
+		retval = true;
+	}
+	return(retval);
+}
+function clearProcessingInput(object) {
+	if($(object).hasClass("processingInput")) {
+		$(object).removeClass("processingInput");
+	}
 }
 
 function ajax_processChange(divId) {
@@ -33,62 +86,107 @@ function ajax_processChange(divId) {
 	
 	ajax_doPost("member/ttorp/character_updates", postArray);
 }
+function ajax_processNewRecord(tableName) {
+	var postArray = {
+		'character_id'	: $("#form_character_id").val()
+	}
+	$("#" + tableName +" .newRecord").each(function(tIndex,tObject) {
+		postArray[$(tObject).attr("id")] = $(tObject).val();
+	});
+	ajax_doPost("member/ttorp/character_updates", postArray);
+}
 function ajax_showUpdatedInput(xmlObj) {
-	$("input").removeClass("yellowBg");
+	$("input").removeClass("updatedInput");
+	var forceNameChange=null;
+	if($(xmlObj).find('id_was').text()) {
+		forceNameChange = $(xmlObj).find('id_was').text();
+	}
 	if($(xmlObj).find('changesbykey').text()) {
 		
 		for (var iNode = 0; iNode < xmlObj.childNodes.length; iNode++) {
-		   var node = xmlObj.childNodes.item(iNode);
-		   for (i = 0; i < node.childNodes.length; i++) {
-		      var sibling = node.childNodes.item(i);
-		      for (x = 0; x < sibling.childNodes.length; x++) {
-		         var sibling2 = sibling.childNodes.item(x);
-		         if (sibling2.childNodes.length > 0) {
-		            var sibling3 = sibling2.childNodes.item(0);
-		            
-		            $("#" + sibling2.nodeName).val(sibling3.data);
-		            $("#" + sibling2.nodeName).addClass("yellowBg");
-		         }
-		      }
-		   }
+			var node = xmlObj.childNodes.item(iNode);
+			for (i = 0; i < node.childNodes.length; i++) {
+				var sibling = node.childNodes.item(i);
+				for (x = 0; x < sibling.childNodes.length; x++) {
+					var forceChange = false;
+					var sibling2 = sibling.childNodes.item(x);
+					if(sibling2.nodeName == forceNameChange) {
+						forceChange=true;
+					}
+					if (sibling2.childNodes.length > 0) {
+						var sibling3 = sibling2.childNodes.item(0);
+						markUpdatedInput($("#" + sibling2.nodeName), sibling3.data, forceChange);
+					}
+					else if(sibling2.nodeName.match(/^[aZ-zZ]/)) {
+						//This handles clearing-out data.
+						markUpdatedInput($("#" + sibling2.nodeName), "", forceChange);
+					}
+				}
+			}
 		}
 	}
 }
 
+function processChange(object) {
+	if(isDirtyInput(object) && $(object).not(".newRecord")) {
+		//NOTE::: the change MUST be submitted before marking it as being processed so "new" records will work properly.
+		$("#"+ $(object).attr('id')).attr('readonly', 'readonly');
+		ajax_processChange($(object).attr('id'));
+		markProcessingInput(object);
+	}
+}
+function handlePendingChanges() {
+	//this will submit changes for all pending (dirty) inputs (that are NOT new records).
+	$("input.dirtyInput").not(".newRecord").each(function(item, object){
+		//alert("found dirty input: "+ $(object).attr("id") +")");
+		processChange(object);
+	});
+}
+
 
 $(document).ready(function() {
+	$("input,select,textarea").each(function(i) {
+		  $(this).data('last_value', $(this).val());
+	});
+	
+	$("input,select,textarea").keyup(function() {
+		if ($(this).val() != $(this).data('last_value')) {
+			markDirtyInput(this);
+		}
+	});
+	$("input,select,textarea").bind('paste', function() {
+		//alert("Something was pasted... (" + $(this).attr("id") + ")");
+		markDirtyInput(this);
+	});
+	$("input,select,textarea").blur(function() {
+		processChange(this);
+	});
+	
+	/*
 	$("input").focus(function(event) {
 		//store the original value so the AJAX is called only when something has actually changed 
 		//	(protects against submitting non-changes when user presses <tab>, etc)
-		originalValue = $(this).val();
+		//originalValue[$(this).attr("id")] = $(this).val();
 	});
 	$("textarea").focus(function(event) {
-		originalValue = $(this).val();
+		//originalValue[$(this).attr("id")] = $(this).val();
 	});
 	$("input").keyup(function(event) {
-		if($(this).val() != originalValue) {
-			markDirtyInput(this);
-		}
+		setInputDirty(this);
 	});
+	//TODO: make this event work!!!
 	$("select").keyup(function(event) {
-		ajax_processChange($(this).attr('id'));
+		//ajax_processChange($(this).attr('id'));
+		processChange(this);
 	});
 	$("input").blur(function() {
-		
-		if(isDirtyInput(this)) {
-			ajax_processChange($(this).attr('id'));
-		}
-		clearDirtyInput(this);
+		processChange(this);
 	});
-	$("textarea").keypress(function(event) {
-		if($(this).val() != originalValue) {
-			markDirtyInput(this);
-		}
+	$("textarea").change(function(event) {
+		markDirtyInput(this);
 	});
 	$("textarea").blur(function() {
-		if(isDirtyInput(this)) {
-			ajax_processChange($(this).attr('id'));
-		}
-		clearDirtyInput(this);
+		processChange(this);
 	});
+	//*/
 });
