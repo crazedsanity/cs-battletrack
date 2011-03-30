891bfca8c56aab1ee84764f49cfe95c1f459cda9
crazedsanity <crazedsanity@4b77e13d-ec49-0410-873c-e52f5c533875>
2011-03-30 04:58:29 +0000

    Object-oriented javascript programming to move pieces around & such.
    Working on how to add a value to the alpha "z" so when they move one to the right of "z1", the program would calculate it as "aa1" instead of "za1"... but, then, maybe it should just calculate as "26-1" moving to "27-1"...

--- trunk/0.5/sample_scripts/js/map.js
+++ trunk/0.5/sample_scripts/js/map.js
@@ -1,32 +1,105 @@
 function mapStorage() {
-	this.selectedPiece = null;
-	this.pieces = new Array();
+	var _selectedPiece = null;
+	var _pieces = new Array();
+	var _locations = new Array();
+	
+	// Identifiers...
+	var _lastPieceId = null;
+	
+	/** Add a piece by name, get an ID. */
+	this.AddPiece = function(pName, pLocation) {
+		if(_lastPieceId == null) {
+			_lastPieceId = 0;
+		}
+		else {
+			_lastPieceId++;
+		}
+		_pieces[_lastPieceId] = pName;
+		_locations[_lastPieceId] = pLocation;
+		
+		//add the piece to the map.
+		$("#"+ pLocation).html(pName);
+		return(_lastPieceId);
+	}//end AddPiece()
+	
+	/** Retrieve a piece's name based on an ID. */
+	this.GetPieceById = function(pId) {
+		return(_pieces[pId]);
+	}//end GetPieceById()
+	
+	/**  */
+	this.GetPieceLocation = function(pId) {
+		return(_locations[pId]);
+	}//end GetPieceLocation()
+	
+	
+	/** Move a piece. */
+	this.MovePiece = function(pId, pNewLocation) {
+		var tRetval = false;
+		if(_pieces[pId] != undefined) {
+			//move from the old location.
+			this.MakeSpaceEmpty(_locations[pId]);
+			
+			// update to a new location.
+			_locations[pId] = pNewLocation;
+			
+			$("#"+ _locations[pId]).html(_pieces[pId]);
+			
+			tRetval = true;
+		}
+		return(tRetval);
+	}//end MovePiece()
+	
+	/** Make the space (pId == ID of cell on page) empty */
+	this.MakeSpaceEmpty = function(pId) {
+		$("#"+ pId).html("&nbsp;");
+	}//end MakeSpaceEmpty()
+	
+	/**  */
+	this.CalculateAddAlpha = function(pAddToThis) {
+	}//end CalculateAddAlpha()
+}
+
+function _AlphaCalc() {
+	
+	var _AlphaArr = new Array();
+	
+	this.BuildArray = function() {
+		if(_AlphaArr.length != 26) {
+			_AlphaArr = new Array();
+			var tStr = "abcdefghijklmnopqrstuvwxyz";
+			var tBits = tStr.split("");
+			for(i=0;i<tBits.length;i++) {
+				var x = (i+1);
+				var ttChar= tBits[i];
+				_AlphaArr[ttChar] = x;
+				//alert("i=("+ i +"), x=("+ x +"), ttChar=("+ ttChar +")");
+			}
+			//alert("done... test 'a'=("+ _AlphaArr['a'] +")");
+		}
+	}//end BuildArray()
+	
+	this.AlphaToNum = function(pStr) {
+		this.BuildArray();
+		tRetval = 0;
+		var tBits = pStr.split("");
+		
+		for(var i=0; i<tBits.length;i++) {
+			var tIndex = tBits[i];
+			tRetval += _AlphaArr[tIndex];
+		}
+		return(tRetval);
+	}//end AlphaToNum()
 }
 
 //create an object
 _map = new mapStorage();
+obj = new _AlphaCalc();
 
-function space_makeEmpty(id) {
-	$("#"+ id).html("&nbsp;");
-}
-
-function space_fill(pId, pHtml) {
-	$("#"+ pId).html(pHtml);
-}
-
-function piece_add(pHtml, pId) {
-	space_fill(pId, pHtml);
-}
-
-function piece_move(oldId, newId){
-	var tMyData = $("#"+ oldId).html();
-	space_makeEmpty(oldId);
-	piece_add(tMyData, newId);
-}
 
 $(document).ready(function(){
 	$("table.ttorp tr td").each(function(){
 		var tMyId = $(this).attr("id");
-		space_makeEmpty(tMyId);
+		_map.MakeSpaceEmpty(tMyId);
 	});
 });
