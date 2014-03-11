20ab4e5346d92d84876352c5c3d584b9f3aed5de
Dan Falconer <github@crazedsanity.com>
2014-03-11 13:21:03 -0500

    Moved files, add template for creating tests

---
+++ tests/_TestTemplate.php
@@ -0,0 +1,36 @@
+<?php
+
+class testOfCSBattleTrack extends testDbAbstract {
+	
+	//--------------------------------------------------------------------------
+	function setUp() {
+		
+		$this->gfObj = new cs_globalFunctions;
+		$this->gfObj->debugPrintOpt=1;
+		
+		parent::setUp();
+		$this->reset_db();
+		$this->dbObj->load_schema($this->dbObj->get_dbtype(), $this->dbObj);
+		$this->dbObj->run_sql_file(dirname(__FILE__) .'/../docs/sql/tables.sql');
+	}//end setUp()
+	//--------------------------------------------------------------------------
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function tearDown() {
+		parent::tearDown();
+	}//end tearDown()
+	//--------------------------------------------------------------------------
+	
+	
+}
+
+class _test_character extends csbt_character {
+	public $_data;
+	
+	public function _clean_data_array() {
+		return parent::_clean_data_array();
+	}
+}
+
